0ac38368f1388f7a2580990234b0332ee97f2d1a
ajeetsharma389 <ajeetsharma389@gmail.com>
2022-05-04 15:53:55 +0530

    print statement added

--- vars/evenOdd.groovy
+++ vars/evenOdd.groovy
@@ -1,6 +1,10 @@
 def call(Map config) {
+    println('EvenOdd')
+    println(config)
     if(env.BUILD_NUMBER.toInteger() % 2 == 0) {
+        println('Number is Even')
         return true
     }
+     println('Number is Odd')
     return false
 }
